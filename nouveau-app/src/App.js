691ea3c48c566a1d7d76f9aaabf5f378c82d6c8b
Evan Jackson <evanjack@iu.edu>
2023-09-28 17:35:28 -0400

    Created basic registration form with api calls to validate input and send user info to backend

--- nouveau-app/src/App.js
+++ nouveau-app/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button} from 'reactstrap'
+import {Link} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Landing page</h1>
+        <Link to={"/Registration"}>
+          <Button>register</Button>
+        </Link>
     </div>
   );
 }
--- nouveau-app/src/index.js
+++ nouveau-app/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { 
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Registration from './routes/Registration';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +13,12 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/registration" element={<Registration />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
